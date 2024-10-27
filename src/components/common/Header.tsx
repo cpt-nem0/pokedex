4fc6f49161c98d10c6e51d88a76ca0fe1cfbb4b4
Rohan <rohanya.76426@gmail.com>
2024-10-28 01:32:51 +0530

    added svg in builds

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,13 +1,11 @@
 import NavBar from "./NavBar";
+import pokemonSvg from "../../assets/pokemon.svg?url";
 
 const Header: React.FC = () => {
   return (
     <header className="w-full flex justify-between pb-2 px-10 items-center pt-8">
       <div className="w-40 h-auto relative">
-        <img
-          src="/src/assets/pokemon.svg"
-          className="w-full h-full object-contain"
-        />
+        <img src={pokemonSvg} className="w-full h-full object-contain" />
       </div>
       <div className="absolute left-1/2 transform -translate-x-1/2">
         <NavBar />
