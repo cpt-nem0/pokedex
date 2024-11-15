acb86d109ef94c2184209a78beb0a2d2b8f378d6
Rohan <rohanya.76426@gmail.com>
2024-11-16 02:22:47 +0530

    made homepage responsive

--- src/components/pages/HomePage/Image.tsx
+++ src/components/pages/HomePage/Image.tsx
@@ -16,11 +16,11 @@ const Image: React.FC<ImageProp> = ({ url, cryUrl }) => {
 
   return (
     <div
-      className="flex-2 min-w-[500px] w-[140px] md:w-[160px] h-auto relative aspect-square"
+      className="flex-2 min-w-[200px] md:min-w-[500px] w-auto md:w-[160px] h-auto relative aspect-square"
       onClick={handleAudioPlay}
     >
       <img src={url} className="w-full h-full object-contain" />
-      <div className="w-full h-[90px] border-2 border-[#F1F0F5] rounded-[100%]"></div>
+      <div className="w-full h-[90px] border-2 border-[#F1F0F5] rounded-[100%] hidden md:block"></div>
       <audio src={cryUrl} ref={audioRef}></audio>
     </div>
   );
--- src/components/pages/HomePage/PokemonDetails.tsx
+++ src/components/pages/HomePage/PokemonDetails.tsx
@@ -19,7 +19,7 @@ const Attributes: React.FC<AttributesProps> = ({ label, types }) => {
   return (
     <div className="flex flex-col gap-2">
       <h2 className="text-2xl uppercase">{label}</h2>
-      <div className="flex gap-4">
+      <div className="flex gap-2 md:gap-4 flex-wrap">
         {types.map((type, idx) => (
           <PokemonType key={idx} typeName={type} />
         ))}
@@ -35,13 +35,13 @@ const PokemonDetails: React.FC<PokemonDetailsProps> = ({
   types,
 }) => {
   return (
-    <div className="flex-1 flex flex-col gap-8 pl-8">
-      <div className="flex flex-col gap-6">
+    <div className="flex-1 flex flex-row md:flex-col gap-2 md:gap-8 justify-center">
+      <div className="flex flex-1 flex-col gap-6 min-w-[200px] pl-4 md:pl-0">
         <Attributes label="Type" types={types} />
         <Attributes label="Weak Against" types={weakAgainst} />
         <Attributes label="Strong Against" types={strongAgainst} />
       </div>
-      <div className="flex flex-col gap-2.5 text-sm">
+      <div className="flex flex-1 flex-col gap-2.5 text-sm min-w-[200px] pl-10 md:pl-0">
         {Object.entries(pokemonStats).map(([stat, value]) => (
           <div key={stat}>
             <p className="uppercase">{`${stat}: ${value}`}</p>
